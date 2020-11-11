f7716f60f26120f5ee186c60139b1a4842fe27b0
nipun gera <nipungera7777@gmail.com>
2020-11-11 23:36:34 +0530

    11/11/2020

---
+++ operators.py
@@ -0,0 +1,37 @@
+a=3
+b=5
+
+# Use Of Operators
+# In python * means multiply
+print("value of 3+4 is ",3+4) # Addition
+print("value of 3+4 is ",3-4) # Subtraction
+print("value of 3+4 is ",3*4) #Multiplication
+print("value of 3+4 is ",3/4) #Division
+
+
+# ASSIGNMENT OPERATORS
+a=36
+a+=3
+print(a)
+
+
+
+# COMPARISION OPERATOR
+
+b=(14!=7)
+c=(9 == 2)
+
+print(b)
+print(c)
+
+# LOGICAL OPERATOR
+
+bool1=True
+bool2=False
+
+
+print("The value of bool1 and bool2 is", bool1 and bool2 )
+print("The value of bool1 or bool2 is", bool1 or bool2 )
+print("The value of not bool2 is ", not bool2 )
+
+
---
+++ program.py
@@ -0,0 +1,22 @@
+# Declaring the variable
+a='nipun'
+b=45
+c=34.45
+d = True
+e=None   # kuch nhi ko none bolte
+
+# Printing the variable
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
+#Printing Type of Variable
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
