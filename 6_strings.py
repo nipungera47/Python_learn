45518fb59adfc1c85b28db5571d5bc26aa21bd24
nipun gera <nipungera7777@gmail.com>
2020-11-12 23:36:53 +0530

    11.12.2020/strings

---
+++ 6_strings.py
@@ -0,0 +1,48 @@
+# This full page is dedicated to strings
+
+
+a=40
+b="Nipun"
+
+print(a,b)
+print(type(b))
+
+#The reason and implementation why single,double  and triple quotes are used
+
+c="ni'pun'nfns"
+print(c)
+
+d=''' ""My name is nipun gera "'''
+# so here we can see the double and single quotes are printed when we execute the program.....
+print(d)
+
+
+
+
+#STRING CONCATINATION....
+
+str1="Good morning"
+str2="How are you"
+
+print(str1 + str2)
+
+
+# STRING SLICING ...
+
+name="nipun"
+
+print(name[0])
+
+print(name[1])
+
+print(name[2])
+
+print(name[3])
+
+print(name[4])
+
+
+print(name[0:3])
+
+
+
