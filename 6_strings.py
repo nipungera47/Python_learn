02d63d52af77b8d91de351f8de8b80501ee2006f
nipun gera <nipungera7777@gmail.com>
2020-11-13 17:02:06 +0530

    11/13/2020

--- 6_strings.py
+++ 6_strings.py
@@ -44,5 +44,47 @@ print(name[4])
 
 print(name[0:3])
 
+print(name[1:4])
+
+print(name[:3])  # it will automatically replace the blank place with the 0 index here!!
+
+#similar if we leave end index blank then we will have the string printed from the starting index till the end index..
+
+print(name[1:]) # OUTPUT WILL BE ipun  same as [1:N-1]...
+
+#Negetive indices are also used for slicing of the string
+
+print(name[-5:-1]) # here we will get ___name[0:4]___ in the output ....
 
 
+#slicing using skip value ....
+print(name[1:2:5])
+
+
+#2nd example
+
+name1="MynameisNipunGera"
+
+e= name1[1:7:2]
+
+print(e)
+
+
+
+
+
+# String Functions
+
+str3="i am a very good boy"
+
+print(len(str3))
+
+print(str3.count("a"))
+
+print(str3.endswith("boy"))  #this will give answer in the form of boolean
+
+print(str3.capitalize())
+
+print(str3.find("very"))  # this will find theword if it is there
+
+print(str3.replace("very","asdf"))  # this will replace the word
