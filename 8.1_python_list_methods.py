6798b626d1c7ea827f47d72fc0436229ff26ccdf
nipun gera <nipungera7777@gmail.com>
2020-11-15 11:42:47 +0530

    11/15/2020 list

---
+++ 8.1_python_list_methods.py
@@ -0,0 +1,38 @@
+
+#List_Sorting
+
+
+
+list=[1,4,2,5,23,43,12,43]
+print(list)
+
+# this function will sort the list
+list.sort()
+
+print(list)
+# this function will reverse the list
+list.reverse()
+print(list)
+
+#this function will append the list
+
+list.append(3)
+print(list)  # you can see in the output that  3 is added at the back of the list
+
+
+# insert() function is used to insert value at an index
+
+list.insert(2,23)
+print(list)
+
+
+# pop fuction is used to delete the value at an index in the list
+
+
+list.pop(2) # value is deleted which is at the index 2
+
+print(list)
+
+# this will remove 43 from the list
+list.remove(43)
+print(list)
---
+++ 8_list_and_tuple.py
@@ -0,0 +1,40 @@
+ # list are container and store in ordered way
+
+# Creste list using []
+a=[1,3,24,423]  # collection of these number
+
+#printing list using print fuction....
+
+print(a)
+
+# Print element value  wise using index ......
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+
+#change the value of the list using
+
+a[0] = 96
+print(a)
+
+
+
+#We can create the list with items of different type
+c=[45,"Nipun",False,9.8]   # we can do this stuff as this is valid.
+
+print(c)
+
+
+#list slicing
+animals=["cat","dog","lion","tiger"]
+print(animals[0:3])
+print(animals[-4:])
+
+
+
+
+
+
+
