e13f9a9d7eec629400d2bdefbe7ad4afeef1fca3
nipun gera <nipungera7777@gmail.com>
2020-11-19 12:28:22 +0530

    11/19/2020

--- 12_practice_set_5.py
+++ 12_practice_set_5.py
@@ -1,14 +1,50 @@
 # This is Practice set 5
 #Q1 find word from dictionary
 
+#
+# dict={
+#     "meetha": "chocolate",
+#     "pyarr":"love",
+#     "television":"tv"
+#
+# }
+#
+# print("The options are :",dict.keys())
+# a=input("Enter the word: ")
+# print("MEaning of the word is:",dict.get(a))
 
-dict={
-    "meetha": "chocolate",
-    "pyarr":"love",
-    "television":"tv"
 
-}
+# Q2 Enter 8 number and put them in the set
+'''
+num1=int(input("Enter number 1"))
+num2=int(input("Enter number 2"))
+num3=int(input("Enter number 3"))
+num4=int(input("Enter number 4"))
+num5=int(input("Enter number 5"))
+num6=int(input("Enter number 6"))
+num7=int(input("Enter number 7"))
+num8=int(input("Enter number 8"))
 
-print("The options are :",dict.keys())
-a=input("Enter the word: ")
-print("MEaning of the word is:",dict.get(a))
+
+s={num1,num2,num3,num4,num5,num6,num7,num8}
+print(s)
+'''
+
+# Q3 What will be the length of set s
+
+s=set()
+print(len(s))
+
+s.add(20)
+
+print(len(s))
+
+s.add(20.0)
+
+print(len(s))
+
+s.add("20")
+print(len(s))
+
+# Q4 Type of s={} is dictionary
+
---
+++ 13_conditionals.py
@@ -0,0 +1,16 @@
+ age=int(input("Enter your Age"))
+#
+# if age>18:
+#     print("Yes")
+#
+# else:
+#     print("no")
+
+
+
+if(age>34 and age<56):
+    print("You can work here")
+
+
+
+    # similarly you can use if, elif and else aaccording to the given conditions
